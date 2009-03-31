5ecad14787ea897b2aa3e001a29341ba5ea73d2f
Kaz Okuda <kaz@okuda.ca>
2009-03-31 03:38:10 +0000

    Add image resources for drawing AND and OR gates. Added the CountintTo255.xml test circuit.

--- Components/And.cs
+++ Components/And.cs
@@ -16,6 +16,14 @@ namespace LogicPuzzle.Components
             Connections[0].Location = new Point(5, 5);
             Connections[1].Location = new Point (5, this.Height - 5);
             Connections[2].Location = new Point(this.Width - 5, this.Height / 2);
+
+            System.IO.Stream stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("LogicPuzzle.Resources.And.png");
+            mBitmap = new Bitmap(stream);
+        }
+
+        ~And()
+        {
+            mBitmap.Dispose();
         }
 
         public override void Execute()
@@ -26,8 +34,22 @@ namespace LogicPuzzle.Components
 
         public override void DrawComponent(Graphics g)
         {
-            base.DrawComponent(g);
-            g.DrawString("AND", new Font("Courier", 10), Brushes.Black, this.Width / 2, this.Height / 2);
+            //base.DrawComponent(g);
+
+            for (int i = 0; i < mConnections.Length; ++i)
+            {
+                Color c = GetValue(i) ? Color.Red : Color.Black;
+                int w = mConnections[i].Connections.Count > 0 ? 2 : 1;
+                Pen pen = new Pen(c, w);
+
+                g.DrawEllipse(pen, new Rectangle(Point.Subtract(mConnections[i].Location, new Size(2, 2)), new Size(4, 4)));
+                g.DrawLine(pen, mConnections[i].Location, new Point(this.Width / 2, mConnections[i].Location.Y));
+            }
+
+            g.DrawImage(mBitmap, 0, 0);
+            //g.DrawString("AND", new Font("Courier", 10), Brushes.Black, this.Width / 2, this.Height / 2);
         }
+
+        Bitmap mBitmap;
     }
 }
--- Components/Or.cs
+++ Components/Or.cs
@@ -16,6 +16,14 @@ namespace LogicPuzzle.Components
             Connections[0].Location = new Point(5, 5);
             Connections[1].Location = new Point(5, Height - 5);
             Connections[2].Location = new Point(Width - 5, Height / 2);
+
+            System.IO.Stream stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("LogicPuzzle.Resources.Or.png");
+            mBitmap = new Bitmap(stream);
+        }
+
+        ~Or()
+        {
+            mBitmap.Dispose();
         }
 
         public override void Execute()
@@ -26,8 +34,21 @@ namespace LogicPuzzle.Components
 
         public override void DrawComponent(Graphics g)
         {
-            base.DrawComponent(g);
-            g.DrawString("OR", new Font("Courier", 10), Brushes.Black, this.Width / 2, this.Height / 2);
+            for (int i = 0; i < mConnections.Length; ++i)
+            {
+                Color c = GetValue(i) ? Color.Red : Color.Black;
+                int w = mConnections[i].Connections.Count > 0 ? 2 : 1;
+                Pen pen = new Pen(c, w);
+
+                g.DrawEllipse(pen, new Rectangle(Point.Subtract(mConnections[i].Location, new Size(2, 2)), new Size(4, 4)));
+                g.DrawLine(pen, mConnections[i].Location, new Point(this.Width / 2, mConnections[i].Location.Y));
+            }
+
+            g.DrawImage(mBitmap, 0, 0);
+            //base.DrawComponent(g);
+            //g.DrawString("OR", new Font("Courier", 10), Brushes.Black, this.Width / 2, this.Height / 2);
         }
+
+        private Bitmap mBitmap;
     }
 }
